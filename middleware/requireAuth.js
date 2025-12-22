e61e25b9362978faffb163dd96a6a3095c14e112
GreatDeveloper66 <ashaffe@gmail.com>
2025-12-21 19:44:30 -0600

    Add requireAuth middleware

---
+++ middleware/requireAuth.js
@@ -0,0 +1,19 @@
+// middleware/requireAuth.js
+import jwt from "jsonwebtoken";
+
+export function requireAuth(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Missing token" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { id: payload.sub };
+    next();
+  } catch {
+    res.status(401).json({ message: "Invalid token" });
+  }
+}
